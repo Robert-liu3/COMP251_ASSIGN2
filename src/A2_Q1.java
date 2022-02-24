6fdbd22493980896f6556e9f4e291e16efa6aaa7
Robert-liu3 <77682309+Robert-liu3@users.noreply.github.com>
2022-02-24 13:45:11 -0500

    other commmits

--- src/A2_Q1.java
+++ src/A2_Q1.java
@@ -3,42 +3,56 @@ import java.util.*;
 public class A2_Q1 {
 	public static HashMap<Integer, Integer> allo = new HashMap<Integer, Integer>();
 	public static int[] game(String[][] board){
+		int[] game = {0,0};
 		for (int row = 0; row < board[0].length; row++) {
 			for (int col = 0; col < board.length; col++) {
 				if (board[row][col].equals("o")) {
-
+					board = oBeside(board, row, col);
+					if (board[row][col].equals("o")) {
+						game[0] ++;
+					} else game[1] ++;
 				}
 			}
 		}
 	}
+	public static void oGetter(String[][] board, int row, int col) {
+		if (board[row][col].equals)
+	}
 
-	public void oBeside(String[][] board, int row, int col) {
+	public static String[][] oBeside(String[][] board, int row, int col) {
 		if (board[row][col+1].equals("o")) {
-			if (board[row][col+2].equals("o")) {
+			if (board[row][col+2].equals(".")) {
 				board[row][col] = ".";
 				board[row][col + 1] = ".";
 				board[row][col+2] = "o";
+				return board;
 			}
 		}
 		else if (board[row+1][col].equals("o")) {
-			if (board[row+1][col].equals("o")) {
+			if (board[row+1][col].equals(".")) {
 				board[row][col] = ".";
 				board[row + 1][col] = ".";
 				board[row+2][col] = "o";
+				return board;
 			}
 		}
 		else if (board[row-1][col].equals("o")) {
-			if (board[row+1][col].equals("o")) {
+			if (board[row+1][col].equals(".")) {
 				board[row][col] = ".";
 				board[row - 1][col] = ".";
+				board[row-2][col] = "o";
+				return board;
 			}
 		}
 		else if (board[row][col-1].equals("o")) {
-			board[row][col] = ".";
-			board[row][col-1] = ".";
-			oBeside(board, row, col-1);
+			if (board[row][col-2].equals(".")) {
+				board[row][col] = ".";
+				board[row][col - 1] = ".";
+				board[row][col-2] = "o";
+				return board;
+			}
 		}
-		else board[row][col] = "o";
+		return board;
 	}
 
 }
