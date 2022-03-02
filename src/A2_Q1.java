6544b13173bcba7db4ab9ca3faf9c58ea0a9be8f
Robert-liu3 <77682309+Robert-liu3@users.noreply.github.com>
2022-03-02 18:49:02 -0500

    added changes

--- src/A2_Q1.java
+++ src/A2_Q1.java
@@ -1,59 +1,71 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class A2_Q1 {
 	public static ArrayList<ArrayList<Integer>> coords = new ArrayList<ArrayList<Integer>>();
+	public static String[][] real_board;
+
 	public static int[] game(String[][] board){
-		int[] game = {0,0};
-		for (int row = 0; row < board[0].length; row++) {
-			for (int col = 0; col < board.length; col++) {
-				if (board[row][col].equals("o")) {
+		real_board = board;
+		oGetter();
+		for (ArrayList<Integer> num_list: coords) {
+			for (ArrayList<Integer> )
+		}
+		return null;
+	}
+	public static void oGetter() {
+		for (int row = 0; row < real_board[0].length; row++) {
+			for (int col = 0; col < real_board.length; col++) {
+				if (real_board[row][col].equals("o")) {
 					ArrayList<Integer> single_coords = new ArrayList<Integer>();
 					single_coords.add(row);
 					single_coords.add(col);
 					coords.add(single_coords);
-
 				}
 			}
 		}
 	}
-	public static void oGetter(String[][] board, int row, int col) {
-		if (board[row][col].equals)
+
+	public static void jump(String direction, int row, int col ) {
+		if (real_board[row][col+1].equals("o") && !real_board[row][col+2].equals("o")) {
+			real_board[row][col] = ".";
+			real_board[row][col + 1] = ".";
+			real_board[row][col + 2] = "o";
+		}
+		if (direction.trim().equals("left")) {
+			real_board[row][col] = ".";
+			real_board[row][col-1] = ".";
+			real_board[row][col-2] = "o";
+		}
+		if (direction.trim().equals("up")) {
+			real_board[row][col] = ".";
+			real_board[row+1][col] = ".";
+			real_board[row+2][col] = "o";
+		}
+		if (direction.trim().equals("down")) {
+			real_board[row][col] = ".";
+			real_board[row-1][col] = ".";
+			real_board[row-2][col] = "o";
+		}
+
 	}
 
-	public static String[][] oBeside(String[][] board, int row, int col) {
-		if (board[row][col+1].equals("o")) {
-			if (board[row][col+2].equals(".")) {
-				board[row][col] = ".";
-				board[row][col + 1] = ".";
-				board[row][col+2] = "o";
-				return board;
-			}
+	public static ArrayList<String> oBeside(int row, int col) {
+		ArrayList<String> possibleJumps = new ArrayList<String>();
+		if (real_board[row][col+1].equals("o") && !real_board[row][col+2].equals("o")) {
+			possibleJumps.add("right");
 		}
-		else if (board[row+1][col].equals("o")) {
-			if (board[row+1][col].equals(".")) {
-				board[row][col] = ".";
-				board[row + 1][col] = ".";
-				board[row+2][col] = "o";
-				return board;
-			}
+		else if (real_board[row][col-1].equals("o") && !real_board[row][col-2].equals("o")) {
+			possibleJumps.add("left");
 		}
-		else if (board[row-1][col].equals("o")) {
-			if (board[row+1][col].equals(".")) {
-				board[row][col] = ".";
-				board[row - 1][col] = ".";
-				board[row-2][col] = "o";
-				return board;
-			}
+		else if (real_board[row+1][col].equals("o") && !real_board[row+2][col].equals("o")) {
+			possibleJumps.add("up");
 		}
-		else if (board[row][col-1].equals("o")) {
-			if (board[row][col-2].equals(".")) {
-				board[row][col] = ".";
-				board[row][col - 1] = ".";
-				board[row][col-2] = "o";
-				return board;
-			}
+		else if (real_board[row-1][col].equals("o") && !real_board[row-2][col].equals("o")) {
+			possibleJumps.add("down");
 		}
-		return board;
+
+		return possibleJumps;
 	}
 
 }
